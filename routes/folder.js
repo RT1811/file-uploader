379673090ae6323aa5950a80aa67908372fff188
RT1811 <ritwick.thebest@gmail.com>
2026-08-31 16:05:49 -0700

    Add folder content page

--- routes/folder.js
+++ routes/folder.js
@@ -32,4 +32,31 @@ router.post("/new", async (req, res, next) => {
     }
 });
 
+router.get("/:id", async (req, res, next) => {
+    if(!req.user) {
+        return res.redirect("/log-in");
+    }
+
+    try{
+        const folder = await prisma.folder.findFirst({
+            where: {
+                id: Number(req.params.id),
+                authorId: req.user.id,
+            },
+            include: {
+                childFolders: true,
+                files: true,
+            },
+        });
+
+        if (!folder) {
+            return res.status(404).send("Folder not found");
+        }
+
+        res.render("folders/show", { folder });
+    } catch(err) {
+        next(err);
+    }
+});
+
 export default router;
