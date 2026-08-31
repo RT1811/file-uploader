f820f403f10843efa6039acc66cc9168f0f11797
RT1811 <ritwick.thebest@gmail.com>
2026-08-31 16:28:09 -0700

    Add folder nesting

--- routes/folder.js
+++ routes/folder.js
@@ -7,7 +7,7 @@ router.get("/new", (req, res) => {
     if (!req.user) {
         return res.redirect("/log-in");
     }
-    res.render("folders/form");
+    res.render("folders/form", { parentFolderId: null, });
 })
 
 router.post("/new", async (req, res, next) => {
@@ -54,6 +54,49 @@ router.get("/:id", async (req, res, next) => {
         }
 
         res.render("folders/show", { folder });
+    } catch(err) {
+        next(err);
+    }
+});
+
+router.get("/:id/new", (req, res) => {
+    if (!req.user) {
+        return res.redirect("/log-in");
+    }
+
+    res.render("folders/form", {
+        parentFolderId: req.params.id,
+    });
+});
+
+router.post("/:id/new", async (req, res, next) => {
+    if (!req.user) {
+        return res.redirect("/log-in");
+    }
+
+    try {
+        const { name } = req.body;
+
+        const parentFolder = await prisma.folder.findFirst({
+            where: {
+                id: Number(req.params.id),
+                authorId: req.user.id,
+            },
+        });
+
+        if(!parentFolder) {
+             return res.status(404).send("Folder not found");
+        }
+
+        await prisma.folder.create({
+            data: {
+                name: req.body.name,
+                authorId: req.user.id,
+                parentFolderId: parentFolder.id,
+            },
+        });
+
+        res.redirect(`/folders/${parentFolder.id}`);
     } catch(err) {
         next(err);
     }
