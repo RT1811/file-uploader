83ada146d6cfbabce185703ea4f51c0ddfcba2c7
RT1811 <ritwick.thebest@gmail.com>
2026-08-31 19:42:44 -0700

    Add file type validation

--- config/multer.js
+++ config/multer.js
@@ -1,9 +1,23 @@
 import multer from "multer";
 
+const allowedTypes = [
+    "text/plain",
+    "application/pdf",
+    "image/png",
+    "image/jpeg",
+]
+
 const upload = multer({
     dest: "uploads/",
     limits: {
         fileSize: 5 * 1024 * 1024,
+    },
+    fileFilter: (req, file, cb) => {
+        if (allowedTypes.includes(file.mimetype)) {
+            cb(null, true);
+        } else {
+            cb(new Error("Unsupported file type"));
+        }
     },
 });
 
